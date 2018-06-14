52f6e07803b507f35fe103f5692bf6d57f414297
yarynayavor <yourhurt09@gmail.com>
2018-06-15 00:27:23 +0300

    added base testing, show h2 titles in articles

--- service.js
+++ service.js
@@ -9,11 +9,36 @@ var webdriver = require ('selenium-webdriver'),
     var driver=new webdriver.Builder().forBrowser('chrome').build();
 
     driver.get('https://theukrainians.org');
-    // driver.sleep(100000);
-    // driver.quit();
     
+
     MongoClient.connect(url, function(err, db) {
       if (err) throw err;
       console.log("Database created!");
+      
       db.close();
     });
+    //driver.sleep(10000);
+    //driver.quit();
+
+    try {
+      let k=0;
+      driver.findElements(By.xpath("//div[@class='news_box']//h2[@class='news_title']")).then(function(elements){
+        elements.forEach(function (element) {
+          element.getText().then(function(text){
+              if(text!=="") {
+                k++;
+                console.log(text+k);
+              }
+          });
+        });
+    });
+   
+      // var elements = driver.findElements(By.xpath("//div[@class='news_box']"));
+    } catch (err) {
+      console.log(err);
+    }
+
+    // elements.then(res=>{
+    //   console.log(res);
+    // })
+    
